740faeb4b4d821aaa8fe1dcc0efdb0ceef18e285
shitij_kpn <38817435+shitij-kpn@users.noreply.github.com>
2021-04-06 23:28:12 +0530

    Add files via upload

--- runffmpeg.sh
+++ runffmpeg.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 fileNames=`ls *.mp4`
+OIFS="$IFS"
+IFS=$'\n'
 for file in $fileNames
 do
   file_name=`basename $file .mp4`
-  mkdir "video $file_name"
-  mv $file "video $file_name"/
-  cd "video $file_name"
+  mkdir "$file_name"
+  mv $file "$file_name"/
+  cd "$file_name"
   ffmpeg -i "$file" -c:a aac -strict experimental -c:v libx264 -s 320x180 -f hls -hls_list_size 1000000000 -hls_time 10 180_out.m3u8
-	sleep 10
+	sleep 5
   ffmpeg -i "$file" -c:a aac -strict experimental -c:v libx264 -s 480x270 -f hls -hls_list_size 1000000000 -hls_time 10 270_out.m3u8
-	sleep 10
+	sleep 5
   ffmpeg -i "$file" -c:a aac -strict experimental -c:v libx264 -s 640x360 -f hls -hls_list_size 1000000000 -hls_time 10 360_out.m3u8
-	sleep 10
+	sleep 5
   ffmpeg -i "$file" -c:a aac -strict experimental -c:v libx264 -s 960x540 -f hls -hls_list_size 1000000000 -hls_time 10 540_out.m3u8
-	sleep 10
+	sleep 5
   ffmpeg -i "$file" -c:a aac -strict experimental -c:v libx264 -s 1280x720 -f hls -hls_list_size 1000000000 -hls_time 10 720_out.m3u8
   cd ..
   sleep 1m
